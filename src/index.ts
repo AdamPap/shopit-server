a9144ecba52110c8fd054b38de37e55eff171f6f
Adam <adampapamast@hotmail.com>
2022-12-02 15:47:45 +0200

    add: userRoutes, errorHandler and session config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
 import express from "express";
 import "dotenv-safe/config";
+import "express-async-errors";
+// import cors from "cors";
+import session from "express-session";
+import userRoutes from "./routes/users";
+import { errorHandler } from "./middleware/error-handler";
 
 const app = express();
 
-app.get("/", (_, res) => {
-  res.send("Hello World");
-});
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+app.use(express.json());
+
+app.use(
+  session({
+    name: "qid",
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 365, //1 year
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production" ? true : false,
+      sameSite: "lax", //lax
+      // domain: __prod__ ? ".next-journey.xyz" : undefined,
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(userRoutes);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
